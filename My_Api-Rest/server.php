145fadd9f249f3b8feebaa9696af652faa87f2d9
rgg7888 <rgg7888@outlook.com>
2021-04-22 16:15:00 -0600

    agregando el metodo delete al servidor api

--- My_Api-Rest/server.php
+++ My_Api-Rest/server.php
@@ -62,5 +62,9 @@ switch(strtoupper($_SERVER['REQUEST_METHOD'])) {
         }
         break;
     case 'DELETE':
+        if(!empty($resourceId) && array_key_exists($resourceId, $books)) {
+            unset($books[$resourceId]);
+        }
+        echo json_encode($books);
         break;
 }
