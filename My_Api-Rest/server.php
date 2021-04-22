04e5909c8b719eeb31b6a1ed49f6239d6483d9d1
rgg7888 <rgg7888@outlook.com>
2021-04-22 17:29:29 -0600

    autenticacion via tokens

--- My_Api-Rest/server.php
+++ My_Api-Rest/server.php
@@ -1,43 +1,89 @@
 <?php
 
-/*user y password harcodeado
+/*
+    user y password harcodeado
 
-mecanismo HTTP OUT
+    mecanismo HTTP OUT
 
-$user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['PHP_AUTH_USER'] : '';
-$password = array_key_exists('PHP_AUTH_PW', $_SERVER) ? $_SERVER['PHP_AUTH_PW'] : '';
+    asi se hace la peticion con este metodo :
 
-if($user !== 'rami' || $password !== '1234') {
-    die;
-}
+    curl http://rami:1234@localhost:8000/books
+
+    $user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['PHP_AUTH_USER'] : '';
+    $password = array_key_exists('PHP_AUTH_PW', $_SERVER) ? $_SERVER['PHP_AUTH_PW'] : '';
+
+    if($user !== 'rami' || $password !== '1234') {
+        die;
+    }
 */
 
 
-//mecanismo HMAC, mas seguro
+/*
+    mecanismo HMAC, mas seguro
 
-if(
-    !array_key_exists('HTTP_X_HASH', $_SERVER) ||
-    !array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) ||
-    !array_key_exists('HTTP_X_UID', $_SERVER)
-){
-    echo "no hay encabezado de autenticacion\n";
-    die;
-}
 
-list($hash, $uid, $timestamp) = [
-    $_SERVER['HTTP_X_HASH'],
-    $_SERVER['HTTP_X_UID'],
-    $_SERVER['HTTP_X_TIMESTAMP']
-];
+    asi se hace la peticion con este metodo :
 
-$secret = 'Sh!! No se lo cuentes a nadie!';
+    php generate_hash.php 1
+    curl http://localhost:8000/books -H 'X-HASH: 789fa3fc42dc6b7b1f196f5cb623b08006558660' -H 'X-UID: 1' -H 'X-TIMESTAMP: 1619131663'
 
-$newHash = sha1($uid.$timestamp.$secret);
+    if(
+        !array_key_exists('HTTP_X_HASH', $_SERVER) ||
+        !array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) ||
+        !array_key_exists('HTTP_X_UID', $_SERVER)
+    ){
+        echo "no hay encabezado de autenticacion\n";
+        die;
+    }
 
-if($newHash !== $hash) {
-    echo "el hash no coincide\n";
-    die;
-}
+    list($hash, $uid, $timestamp) = [
+        $_SERVER['HTTP_X_HASH'],
+        $_SERVER['HTTP_X_UID'],
+        $_SERVER['HTTP_X_TIMESTAMP']
+    ];
+
+    $secret = 'Sh!! No se lo cuentes a nadie!';
+
+    $newHash = sha1($uid.$timestamp.$secret);
+
+    if($newHash !== $hash) {
+        echo "el hash no coincide\n";
+        die;
+    }
+*/
+
+/*mecanismo comunicacion via access tokens
+
+    asi se hace la peticion con este metodo :
+
+    curl http://localhost:8001 -X 'POST' -H 'X-Client-Id: 1' -H 'X-Secret: SuperSecreto!'
+    curl http://localhost:8000/books -H 'X-Token: c4b02a1525349e7888d4140dcd524aff2d6296dd'
+
+*/
+    if(!array_key_exists('HTTP_X_TOKEN', $_SERVER)) {
+        die;
+    }
+
+    $url = 'http://localhost:8001';
+    $ch = curl_init($url);
+    curl_setopt(
+        $ch,
+        CURLOPT_HTTPHEADER,
+        [
+            "X-Token: {$_SERVER['HTTP_X_TOKEN']}"
+        ]
+    );
+    curl_setopt(
+        $ch,
+        CURLOPT_RETURNTRANSFER,
+        true
+    );
+    $ret = curl_exec($ch);
+    if($ret !== 'true'){
+        echo "error de autenticacion\n";
+        die;
+    }
+#
 
 #definimos los recursos disponibles
 $allowedResourceTypes = [
